4f2cd96679a30f41fca5c131e763edf1e3003ca8
Christopher Wood <cjwood032@gmail.com>
2018-06-19 14:12:36 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,4 +1,11 @@
 def greeting(name)
+<<<<<<< HEAD
   name=#gets.strip.chomp
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+=======
+puts "Hi! Welcome to the wonderful world of Ruby programming."
+puts "Please enter your name so that we can greet you more personally:"
+name = gets.strip
+greeting(name)
+>>>>>>> 874b0a20ef42f3a9ee121270f423b4122b4f4135
 end
